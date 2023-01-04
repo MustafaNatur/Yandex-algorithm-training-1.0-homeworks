b2f8cb60cb35d425752e1c969466159b9eb7a53b
MsustafaNatur <mustafa.natur@mail.ru>
2023-01-04 17:58:22 +0300

    main: added H.swift

---
+++ 2.0/Swift/H.swift
@@ -0,0 +1,15 @@
+// not O(n) solution
+
+// let str = readLine()!
+
+// var arr = str.split(separator: " ").map{Int($0)!}
+// var n = arr.count
+
+// if (n >= 3) {
+// 	arr.sort(by: <)
+// 	if (arr[n - 1] * arr[n - 2] * arr[n - 3] > arr[0] * arr[1] * arr[n - 1]) {
+// 		print("\(arr[n - 1]) \(arr[n - 2]) \(arr[n - 3])")
+// 	} else {
+// 		print("\(arr[n - 1]) \(arr[1]) \(arr[0])")
+// 	}
+// }
