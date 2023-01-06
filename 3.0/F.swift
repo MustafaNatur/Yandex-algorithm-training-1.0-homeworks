766d57081a305532d197997cf49a725141cdc723
MsustafaNatur <mustafa.natur@mail.ru>
2023-01-06 23:54:26 +0300

    main: added F.swift solution

--- 3.0/F.swift
+++ 3.0/F.swift
@@ -1,12 +1,34 @@
-import Foundation
+// Task: F.Alien Genome
+// Compiler: Swift 5.3
+// Verdict: Execution time limit exceeded
+// Status: Partial solution
 
+// import Foundation
+
+// let arr1 = Array(readLine()!).map{String($0)}
+// let str2 = readLine()!
+
+// var counter = 0
+
+// for i in 0..<arr1.count - 1 {
+//     if (str2.contains(arr1[i] + arr1[i + 1])) {
+//         counter+=1
+//     }
+// }
+
+// print(counter)
+
+
+// 100% OK solution
 let arr1 = Array(readLine()!).map{String($0)}
-let str2 = readLine()!
-
+var arr2:Set<String> = []
+let Input = Array(readLine()!).map{String($0)}
+	for i in 1..<Input.count{
+    	arr2.insert(Input[i - 1]+Input[i])
+    } 
 var counter = 0
-
-for i in 0..<arr1.count - 1 {
-    if (str2.contains(arr1[i] + arr1[i + 1])) {
+for i in 0..<arr1.count-1 {
+    if (arr2.contains(arr1[i]+arr1[i+1])){
         counter+=1
     }
 }
